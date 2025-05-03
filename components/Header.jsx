d827713bba42314d220970aef211b8d4ad5b264c
engineer-dilnawaz <155418947+engineer-dilnawaz@users.noreply.github.com>
2025-05-03 16:57:17 +0500

    capitalize header title for consistency

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header className={`header-container ${isDark ? "dark" : ""}`}>
       <div className="header-content">
         <h2 className="title">
-          <a href="/">Where in the globe?</a>
+          <a href="/">Where in the GLOBE?</a>
         </h2>
         <p
           className="theme-changer"
